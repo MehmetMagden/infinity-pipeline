9e09786f631c50025020bf8263095f75ca1f81b7
Mehmet Magden <mehmetmagden.it@gmail.com>
2026-05-12 08:42:29 -0400

    feat: swisstask app, tests and CI/CD pipeline

--- swisstask/tests/test_app.py
+++ swisstask/tests/test_app.py
@@ -1,5 +1,5 @@
 import os
-os.environ['DATABASE_URL'] = 'sqline:///:memory:'
+os.environ['DATABASE_URL'] = 'sqlite:///:memory:'
 
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'app'))
@@ -19,19 +19,19 @@ def client():
         
 def test_health_Check(client):
     response = client.get('/health')
-    assert response.status_Code == 200
+    assert response.status_code == 200
     assert response.json == {"status": "healthy"}
 
 def test_create_and_get_tasks(client):
     # add new task
-    post_resp = client.post('tasks', json={"title": "Stop Thanos"})
+    post_resp = client.post('/tasks', json={"title": "Stop Thanos"})
     assert post_resp.status_code == 201
     assert post_resp.json["title"] == "Stop Thanos"
     assert post_resp.json["done"] is False
 
     # get the task list and check
     get_resp = client.get('/tasks')
-    assert get_resp.status_Code == 200
+    assert get_resp.status_code == 200
     assert len(get_resp.json) == 1
     assert get_resp.json[0]["title"] == "Stop Thanos"
 
@@ -42,4 +42,4 @@ def test_delete_task(client):
     # then delete it
     del_resp = client.delete('/tasks/1')
     assert del_resp.status_code == 200
-    assert del.resp.json["message"] == "Task  deleted"
+    assert del_resp.json["message"] == "Task deleted"
